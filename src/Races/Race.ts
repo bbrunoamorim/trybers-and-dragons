31233676aac6b7fd418b4ee900ea19fc9dc4a465
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-16 17:04:11 -0300

    update: fixes class Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -7,17 +7,17 @@ export default abstract class Race {
     this._dexterity = dexterity;
   }
 
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
   public static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
 
-  public get name(): string {
-    return this.name;
-  }
-
-  public get dexterity(): number {
-    return this.dexterity;
-  }
-
-  public abstract get maxLifePoint(): number;
+  public abstract get maxLifePoints(): number;
 }
