46726985534f6a0bf6e91774a85a0f8dddb312b5
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-17 14:32:53 -0300

    feat: creates PVP battle

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,21 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(private playerOne: Fighter, private playerTwo: Fighter) {
+    super(playerOne);
+  }
+
+  private fighting() {
+    while (this.playerOne.lifePoints !== -1
+      && this.playerTwo.lifePoints !== -1) {
+      this.playerOne.attack(this.playerTwo);
+      this.playerTwo.attack(this.playerOne);
+    }
+  }
+
+  public fight(): number {
+    this.fighting();
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
